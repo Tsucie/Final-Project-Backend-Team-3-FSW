8677576a0611748ad4a02925b408d660caf3b73c
Rizky A <44380444+Tsucie@users.noreply.github.com>
2022-06-21 14:33:16 +0700

    edit db production name

--- config/database.js
+++ config/database.js
@@ -32,7 +32,7 @@ module.exports = {
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_prod`,
+    database: `${DB_NAME}`,
     host: DB_HOST,
     dialect: "postgres",
     dialectOptions: {
