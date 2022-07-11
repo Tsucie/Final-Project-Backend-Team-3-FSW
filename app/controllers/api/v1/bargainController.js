161057df32bd02db2329d30239e329e0ca6479e6
Rizky A <44380444+Tsucie@users.noreply.github.com>
2022-07-11 21:27:35 +0700

    Complete module

--- app/controllers/api/v1/bargainController.js
+++ app/controllers/api/v1/bargainController.js
@@ -49,12 +49,11 @@ module.exports = {
         updatedAt: new Date(),
       };
       const addedBargain = await bargainService.create(newBargain);
-      // PR buat notifkasi user siapa? Seller kah atau buyer
-      if (addedBargain) {
-        let makeNotif = {
-
-        }
-      }
+      return res.status(201).json({
+        status: "CREATED",
+        message: "Data penawaran berhasil ditambahkan",
+        data: addedBargain,
+      });
     } catch (error) {
       return res.status(500).json({
         status: "INTERNAL SERVER ERROR",
@@ -62,6 +61,39 @@ module.exports = {
       });
     }
   },
-  async edit(req, res) {},
-  async delete(req, res) {}
+  async edit(req, res) {
+    try {
+      let { price, result } = req.body;
+      if (!req.params.id) {
+        return res.status(400).json({ status: "BAD REQUEST", message: "Data tidak lengkap" });
+      }
+      const updatedBargain = await bargainService.update(req.params.id, {
+        price, result, updatedAt: new Date()
+      });
+      return res.status(201).json({
+        status: "UPDATED",
+        message: "Data penawaran berhasil diubah",
+        data: updatedBargain[1],
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "INTERNAL SERVER ERROR",
+        message: error.message,
+      });
+    }
+  },
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ status: "BAD REQUEST", message: "Data tidak lengkap" });
+      }
+      await bargainService.delete(req.params.id);
+      return res.status(202).json({ status: "ACCEPTED", message: "Data berhasil dihapus" });
+    } catch (error) {
+      return res.status(500).json({
+        status: "INTERNAL SERVER ERROR",
+        message: error.message,
+      });
+    }
+  }
 };
--- app/controllers/api/v1/notificationController.js
+++ app/controllers/api/v1/notificationController.js
@@ -32,5 +32,67 @@ module.exports = {
         message: error.message,
       });
     }
+  },
+  async create(req, res) {
+    try {
+      let { user_id, bargain_id, product_id, message } = req.body;
+      if (!user_id || !message) {
+        return res.status(400).json({ status: "BAD REQUEST", message: "Data tidak lengkap" });
+      }
+      let newNotif = new Object();
+      newNotif.user_id = user_id;
+      if (bargain_id) newNotif.bargain_id = bargain_id;
+      if (product_id) newNotif.product_id = product_id;
+      newNotif.message = message;
+      newNotif.is_read = false;
+      newNotif.datetime = new Date();
+      newNotif.status = 1;
+      newNotif.createdAt = new Date();
+      newNotif.updatedAt = new Date();
+      await notificationService.create(newNotif);
+      return res.status(201).json({
+        status: "CREATED",
+        message: "Notifikasi berhasil ditambahkan"
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "INTERNAL SERVER ERROR",
+        message: error.message,
+      });
+    }
+  },
+  async update(req, res) {
+    try {
+      let { message, is_read } = req.body;
+      if (!req.params.id) {
+        return res.status(400).json({ status: "BAD REQUEST", message: "Data tidak lengkap" });
+      }
+      await notificationService.update(req.params.id, {
+        message, is_read, updatedAt: new Date()
+      });
+      return res.status(201).json({
+        status: "UPDATED",
+        message: "Notifikasi berhasil diubah"
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "INTERNAL SERVER ERROR",
+        message: error.message,
+      });
+    }
+  },
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({ status: "BAD REQUEST", message: "Data tidak lengkap" });
+      }
+      await notificationService.delete(req.params.id);
+      return res.status(202).json({ status: "ACCEPTED", message: "Data berhasil dihapus" });
+    } catch (error) {
+      return res.status(500).json({
+        status: "INTERNAL SERVER ERROR",
+        message: error.message,
+      });
+    }
   }
 };
